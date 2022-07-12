b2e428a35f088ef3eca1db31c0b574f8e9df8f6f
David Maene <davidmened@gmail.com>
2022-07-12 15:37:24 +0200

    integration done of Hospital

--- routes/routes.routes.js
+++ routes/routes.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { AdminsRoutes } from './routes.admins.js';
-import { UserRoutes } from './routes.users.js';
+import { HospitalRoutes, UserRoutes } from './routes.users.js';
 
 export const Routes = express.Router();
             Routes.use("/users", UserRoutes)
             Routes.use("/admins", AdminsRoutes)
+            Routes.use("hospitals", HospitalRoutes)
--- routes/routes.users.js
+++ routes/routes.users.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import { HospitalController } from '../controllers/controllers.hospitals.js';
 import { UsersController } from '../controllers/controllers.users.js';
 
 export const UserRoutes = express.Router()
     UserRoutes.post("/user/signin", UsersController.signin) // connexion utilisateur
     UserRoutes.post("/user/signup", UsersController.signup) // creation de compte utilisateur
     UserRoutes.post("/user/sendsos", UsersController.sendsos) // cas utilisateur envoi un sos
+
+export const HospitalRoutes = express.Router()
+    HospitalRoutes.get("/hospital/list", HospitalController.list)
+    HospitalRoutes.post("/hospital/add", HospitalController.addhospital)
